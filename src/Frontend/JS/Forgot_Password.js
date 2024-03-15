cda9db11ca14ce431b425eb71ae0864d5d651e5e
ahmed badawi <ahmedbad063@gmail.com>
2024-03-16 01:22:32 +0200

    update frontend user files and add new icons

---
+++ src/Frontend/JS/Forgot_Password.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll(".input-field");
+
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
+});
