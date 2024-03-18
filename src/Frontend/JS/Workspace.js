55e91b9fa4978d8f44caca1bf5fcaf3399b95e35
ahmed badawi <ahmedbad063@gmail.com>
2024-03-19 01:37:27 +0200

    update frontend files last update 3/19/2024 1:37 AM

--- src/Frontend/JS/Workspace.js
+++ src/Frontend/JS/Workspace.js
@@ -1,24 +1,21 @@
 const body = document.querySelector("body"),
-      sidebar = body.querySelector(".sidebar"),
-      toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
-      modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-text");
+  sidebar = body.querySelector("nav"),
+  toggle = body.querySelector(".toggle"),
+  searchBtn = body.querySelector(".search-box"),
+  modeSwitch = body.querySelector(".toggle-switch"),
+  modeText = body.querySelector(".mode-text");
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
 
-      toggle.addEventListener("click", () =>{
-        sidebar.classList.toggle("close");
-      });
-      searchBtn.addEventListener("click", () =>{
-        sidebar.classList.remove("close");
-      });
-
-
-      modeSwitch.addEventListener("click", () =>{
-        body.classList.toggle("dark");
-
-        if(body.classList.contains("dark")){
-            modeText.innerText = "Light Mode"
-        }else{
-            modeText.innerText = "Dark Mode"
-        }
-      });
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light mode";
+  } else {
+    modeText.innerText = "Dark mode";
+  }
+});
