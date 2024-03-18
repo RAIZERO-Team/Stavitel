b8804899263e15e532c5eae12cfb4f9a19eda049
shahd <shahd.mahmoud1043@gmail.com>
2024-03-18 23:00:48 +0200

    create sign up form

---
+++ src/Frontend/JS/User_Sign.js
@@ -0,0 +1,35 @@
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+
+inputs.forEach(inp =>{
+    inp.addEventListener("focus", () => {
+        inp.classList.add("active");
+    });
+inp.addEventListener("blur", () => {
+inp.classList.remove("active");
+});
+inp.addEventListener("blur",() => {
+    if(inp.value != "") return;
+    inp.classList.remove("active");
+});
+});
+toggle_btn.forEach(btn =>{
+btn.addEventListener("click",() =>{
+    main.classList.toggle("sign-in-mode");
+});
+});
+function moveSlider(){
+    let index = this.dataset.value;
+    let currentImage = document.querySelector(`.img-${index}`);
+    images.forEach(img => img.classList.remove("show"));
+    currentImage.classList.add("show")
+    bullets.forEach(bull => bull.classList.remove("active"));
+    this.classList.add("active");
+}
+bullets.forEach(bullet => {
+    bullet.addEventListener("click", moveSlider);
+});
+
