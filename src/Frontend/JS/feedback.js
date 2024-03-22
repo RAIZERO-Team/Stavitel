74e11b0482bfcaf9f7df596a3dd1f63e5480424f
ahmed badawi <ahmedbad063@gmail.com>
2024-03-22 10:58:24 +0200

    update files

--- src/Frontend/JS/feedback.js
+++ src/Frontend/JS/feedback.js
@@ -1,5 +1,17 @@
-const btn =document.querySelector(".button");
-const feedback = document.querySelector(".feedback-container");
-btn.addEventListener("click", () => {
-feedback.innerHTML = `<h1>Thank you for your feedback </h1>`;
+const inputs = document.querySelectorAll(".input-field");
+
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
 });
+
+// const btn =document.querySelector(".button");
+// const feedback = document.querySelector(".feedback-container");
+//   btn.addEventListener("click", () => {
+//   feedback.innerHTML = `<h1>Thank you for your feedback </h1>`;
+// });
