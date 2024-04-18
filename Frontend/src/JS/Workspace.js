8c631bbb5ce643e54be3ca5a4c54b15e4d12b687
ahmed badawi <ahmedbad063@gmail.com>
2024-04-18 13:50:41 +0200

    update files

--- Frontend/src/JS/Workspace.js
+++ Frontend/src/JS/Workspace.js
@@ -22,6 +22,39 @@ modeSwitch.addEventListener("click", () => {
 
 // Switch With Sections
 
-const home_section = document.querySelectorAll(".home");
-const my_projects_section = document.querySelectorAll(".my_projects");
+// Function to show the selected section
+function showSection(index) {
+  // Get all sections
+  var sections = document.querySelectorAll(".section");
 
+  // Hide all sections
+  sections.forEach(function (section) {
+    section.classList.remove("active");
+  });
+
+  // Show the selected section
+  sections[index].classList.add("active");
+}
+
+// Show the initial section (index 0) when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+  showSection(0); // Show section 0 by default
+});
+
+// add new project
+
+// Function to open the subpage
+function openSubpage() {
+  var subpageContainer = document.getElementById("subpageContainer");
+  var overlay = document.getElementById("overlay");
+  subpageContainer.style.display = "block";
+  overlay.style.display = "block";
+}
+
+// Function to close the subpage
+function closeSubpage() {
+  var subpageContainer = document.getElementById("subpageContainer");
+  var overlay = document.getElementById("overlay");
+  subpageContainer.style.display = "none";
+  overlay.style.display = "none";
+}
