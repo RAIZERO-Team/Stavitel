46627063b887da14605b06e4ac76375b4186c2d6
yasmeen <yasmeenasaad429@gmail.com>
2024-05-05 22:29:58 +0300

    class dashboard and js.chart

---
+++ Frontend/src/context/dashboard.js
@@ -0,0 +1,11 @@
+class dashboard{
+    constructor(sit_visit, last_projects, project_name,project_type,project_date){
+        this.sit_visit = sit_visit;
+        this.project_type = project_type;
+        this.last_projects = last_projects;
+        this.project_name = project_name;
+        this.project_date = project_date; 
+
+    }
+    async display(){}
+}
