c72e22722a5fa3936d029785a805ff0e46cf9df7
Mahmoud <mahmoudhazem8910@gmail.com>
2024-05-06 23:05:47 +0300

    profile setting

--- Frontend/src/JS/script.js
+++ Frontend/src/JS/script.js
@@ -50,3 +50,17 @@ function showSuggestions(list){
     }
     suggBox.innerHTML = listData;
 }
+
+//---------------------Profile setting-------------------
+let uploadButton = document.getElementById("upload-button");
+let chosenImage = document.getElementById("chosen-image");
+let fileName = document.getElementById("file-name");
+
+uploadButton.onchange = () => {
+  let reader = new FileReader();
+  reader.readAsDataURL(uploadButton.files[0]);
+  console.log(uploadButton.files[0]);
+  reader.onload = () => {
+    chosenImage.setAttribute("src", reader.result);
+  }
+}
