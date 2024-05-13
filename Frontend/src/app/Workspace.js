3f97e1bfd38b6ec3d1c4f96bcb8cc6684ac86faf
ahmed badawi <ahmedbad063@gmail.com>
2024-05-14 02:40:22 +0300

    update files last uspdate 5/14 2:40  AM

--- Frontend/src/app/Workspace.js
+++ Frontend/src/app/Workspace.js
@@ -406,6 +406,35 @@ document.getElementById("removeBtn").addEventListener("click", function () {
   document.getElementById("imageInput").value = null; // Reset input file
 });
 
+// ----- control notfication settings -----
+
+// Get the elements
+const saveButton = document.getElementById('save');
+const cancelButton = document.getElementById('cancel');
+const checkbox = document.getElementById('checkbox');
+
+// Initial state
+let checkboxState = checkbox.checked;
+
+// Add event listeners to buttons
+saveButton.addEventListener('click', saveChanges);
+cancelButton.addEventListener('click', cancelChanges);
+
+// Save changes function
+function saveChanges() {
+  // Update checkbox state
+  checkboxState = checkbox.checked;
+
+  // Simulate saving data (replace with your actual saving logic)
+  console.log('Saving changes:', checkboxState);
+}
+
+// Cancel changes function
+function cancelChanges() {
+  // Restore checkbox state to initial value
+  checkbox.checked = checkboxState;
+}
+
 // ================ Rate Form ================ //
 const allStar = document.querySelectorAll(".rating .star");
 const ratingValue = document.querySelector(".rating input");
